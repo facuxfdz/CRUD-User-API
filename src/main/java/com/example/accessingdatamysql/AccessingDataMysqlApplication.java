6f4354be86c00dd19998ba5e9325fcc72037fde8
Facundo Agustín Fernández <facuassain@gmail.com>
2022-01-09 22:37:03 -0300

    Comments added: entry point java file

--- src/main/java/com/example/accessingdatamysql/AccessingDataMysqlApplication.java
+++ src/main/java/com/example/accessingdatamysql/AccessingDataMysqlApplication.java
@@ -3,7 +3,11 @@ package com.example.accessingdatamysql;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication()
+// Entry point
+@SpringBootApplication() // This annotation can be used to enable three features:
+// @EnableAutoConfiguration: enable Spring boot's auto configuration mechanism (https://docs.spring.io/spring-boot/docs/2.0.x/reference/html/using-boot-auto-configuration.html)
+// @ComponentScan: enable @Component scan on the package where the application is located (Best practices structuring your code: https://docs.spring.io/spring-boot/docs/2.0.x/reference/html/using-boot-structuring-your-code.html)
+// @Configuration: allow to register extra beans in the context or import additional configuration classes
 public class AccessingDataMysqlApplication {
 
 	public static void main(String[] args) {
