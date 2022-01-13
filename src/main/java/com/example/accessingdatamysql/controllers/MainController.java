963e259cb2a99e3bd99bdb28e4c45444a97b5908
Facundo Agustín Fernández <facuassain@gmail.com>
2022-01-13 01:17:42 -0300

    Code refactor
    ResponseBody deleted because its action is inherited

--- src/main/java/com/example/accessingdatamysql/controllers/MainController.java
+++ src/main/java/com/example/accessingdatamysql/controllers/MainController.java
@@ -14,34 +14,33 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
+ 
 @Controller // This means that this class is a controller
 @RequestMapping(path = "/users") // This means URL's start with /users after app path
 public class MainController {
     @Autowired // Allows us to use a service without instantiating it
     private UserService userService; // We sill make our CRUD operations via our userService. In this case is something very simple but it could have any kind of complicated logic inside 
-
+    
     @PostMapping
-    public @ResponseBody User addNewUser(@RequestBody User user) {
+    public User addNewUser(@RequestBody User user) {
         // @ResponseBody means the returned User is the response and not a view name
         return userService.addUser(user);
     }
 
     @GetMapping
-    public @ResponseBody ArrayList<User> getAllUsers() {
+    public ArrayList<User> getAllUsers() {
         // This will return an array of JSON or XML with all users
         return userService.getUsers();
     }
 
     @GetMapping(path = "/{id}")
-    public @ResponseBody Optional<User> getUserById(@PathVariable Integer id) {
+    public Optional<User> getUserById(@PathVariable Integer id) {
         // This will return either the user found or null if search doesn't resolve well
         return userService.getById(id);
     }
 
     @DeleteMapping(path = "/{id}")
-    public @ResponseBody String deleteUserById(@PathVariable Integer id){
+    public String deleteUserById(@PathVariable Integer id){
         boolean deleteQueryOk = userService.deleteUser(id);
         if(deleteQueryOk){
             return "User with id \"" + id + "\" successfully deleted";
